815b850cd51aa0128d2eb0a39dd051aee54f0764
Paul Landers <paul@srednal.com>
2023-03-31 09:52:33 -0400

    Add xcode package template

---
+++ Sources/Bagfile/Bagfile.swift
@@ -0,0 +1,6 @@
+public struct Bagfile {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/BagfileTests/BagfileTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Bagfile
+
+final class BagfileTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Bagfile().text, "Hello, World!")
+    }
+}
