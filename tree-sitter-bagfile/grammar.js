77f5c17a30a75000fc962675a6e3adc4a99381b6
Paul Landers <paul@thelanders.family>
2023-08-29 14:20:49 -0400

    Outline for grammar spec

--- tree-sitter-bagfile/grammar.js
+++ tree-sitter-bagfile/grammar.js
@@ -1,8 +1,35 @@
 module.exports = grammar({
   name: 'bagfile',
 
+  extras: $ => [ $.comment, /\s+/ ],
+
   rules: {
-    // TODO: add the actual grammar rules
-    source_file: $ => 'hello'
+    //// File structure
+    file: $ => repeat1($.tune),
+    tune: $ => seq($.header, optional($.body)),
+    body: $ => repeat1($.measure),
+
+    //// Header and fields
+    header: $ => seq("---\n", repeat(seq($.field, "\n")), "---\n"),
+    field: $ => seq($.label, $.value),
+    inline_field: $ => seq("(", $.field, ")"),
+    label: $ => seq(/[ a-zA-Z]+/, ":"),
+    value: $ => /[ a-zA-Z0-9\/,]+/,
+
+    //// Measures
+    measure: $ => seq(repeat($._measure_element), $.barline),
+    barline: $ => "|",
+    _measure_element: $ => choice($.string, $.inline_field, $.note_cluster),
+
+    //// Note groups
+    note_cluster: $ => seq(repeat1($.note), /\s+/),
+    note: $ => seq(optional($.embellisment), $.pitch, optional($.duration)),
+    embellisment: $ => seq(optional(/[phlxu]/), /[xtvwzkn]{1-2}/),
+    pitch: $ => /[qrbcdefga]/,
+    duration: $ => /[0-9,.\/]+/,
+
+    //// Other
+    string: $ => seq("\"", /[^"]+/, "\""),
+    comment: $ => seq("%", /[^\n]*/)
   }
 });
