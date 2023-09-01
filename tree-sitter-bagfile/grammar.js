2299cc74ebeddcce0731bd40eed0859ff8bcc695
Paul Landers <paul@thelanders.family>
2023-09-01 15:26:24 -0400

    Group body by lines

--- tree-sitter-bagfile/grammar.js
+++ tree-sitter-bagfile/grammar.js
@@ -1,36 +1,47 @@
 module.exports = grammar({
   name: 'bagfile',
 
-  extras: $ => [ $.comment, /\s+/ ],
+  extras: $ => [ /[ \t]+/ ],
 
   rules: {
     //// File structure
-    file: $ => repeat1($.tune),
+    file: $ => seq(optional(repeat($._blank_line)), repeat1($.tune)),
     tune: $ => seq($.header, optional($.body)),
-    body: $ => repeat1($.measure),
+    body: $ => repeat1(choice($._blank_line, $.line)),
+    line: $ => seq(
+                 optional($.inline_field),
+                 optional($.barline),
+                 repeat1($.measure),
+                 optional($.tail_comment),
+                 "\n"),
 
     //// Header and fields
     header: $ => seq("---\n", repeat(seq($.field, "\n")), "---\n"),
     inline_field: $ => seq("(", $.field, ")"),
     field: $ => choice(
-                       field("value", $.field_element),
-                       seq(field("label", $.field_element), ":", field("value", $.field_element))),
-    field_element: $ =>  /[ a-zA-Z0-9\/,]+/, // Consider renaming this
+                  field("value", $.field_element),
+                  seq(
+                    field("label", $.field_element),
+                    ":",
+                    field("value", $.field_element))),
+    field_element: $ =>  /[ a-zA-Z0-9\/,]+/, // TODO: Consider renaming this
 
     //// Measures
-    measure: $ => seq(repeat($._measure_element), $.barline),
-    barline: $ => "|",
-    _measure_element: $ => choice($.string, $.inline_field, $.note_cluster),
+    measure: $ => seq(repeat($._measure_element), prec(-1, $.barline)),
+    barline: $ => /[\[\]|:]+/,
+    _measure_element: $ => choice($.string, prec(1, $.inline_field), $.note_cluster),
 
     //// Note groups
     note_cluster: $ => seq(repeat1($.note), /\s+/),
     note: $ => seq(optional($.embellishment), $.pitch, optional($.duration)),
     embellishment: $ => /[phluxtvwzkn]+/,
     pitch: $ => /[qrbcdefga]/,
-    duration: $ => /[0-9,.\/]+/,
+    duration: $ => /[0-9,.\/-]+/,
 
     //// Other
     string: $ => seq("\"", /[^"]+/, "\""),
-    comment: $ => seq("%", /[^\n]*/)
+    _blank_line: $ => choice("\n", $.comment),
+    comment: $ => seq("%", /[^\n]*/, "\n"),
+    tail_comment: $ => seq("%", /[^\n]*/)
   }
 });
