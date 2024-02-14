2daeac2f3e6950367c2be66a3aef31b15a4c9196
Paul Landers <paul@thelanders.family>
2024-02-13 21:21:55 -0500

    Abc output in progress

--- Sources/BagNotation/Converters/AbcWriter.swift
+++ Sources/BagNotation/Converters/AbcWriter.swift
@@ -9,9 +9,108 @@ private let logger = Logger(subsystem: "bag", category: "AbcWriter")
 
 public class AbcWriter {
     let doc: Doc
+    var abc = ""
+
     public init(_ doc: Doc) { self.doc = doc }
 
     public func makeAbc() throws -> String {
-        "TODO: insert abc output here"
+        doc.tunes.mapToAbc(joined: "\n\n")
+    }
+}
+
+extension Tune: AbcSourceConverting {
+    fileprivate func abcSource() -> String {
+        header.abcSource() + "\n" + lines.mapToAbc(joined: "\n")
+    }
+}
+
+extension Header: AbcSourceConverting {
+    fileprivate func abcSource() -> String {
+        """
+        X:1
+        T:\(title)
+        C:\(composer)
+        R:\(style.abcSource())
+        M:\(timeSignature.rawValue)
+        L:\(noteLength)
+        K:HP
+        """
+    }
+}
+
+extension TuneStyle: AbcSourceConverting {
+    fileprivate func abcSource() -> String {
+        switch self {
+        case .march, .march24, .march34, .march44, .march54, .march68, .march98, .march128:
+            "March"
+        case .hornpipe: "Hornpipe"
+        case .jig, .jig98, .jig128: "Jig"
+        case .reel: "Reel"
+        case .strathspey: "Strathspey"
+        case .slowAir: "Slow Air"
+        case .slowMarch: "Slow March"
+        case .piob: "Piobaireachd"
+        }
+    }
+}
+
+extension Line: AbcSourceConverting {
+    fileprivate func abcSource() -> String {
+        var abc = leadingBarline?.rawValue ?? ""
+        abc += " "
+        abc += bars.mapToAbc(joined: " ")
+        return abc
+    }
+}
+
+extension Bar: AbcSourceConverting {
+    fileprivate func abcSource() -> String {
+        var abc = noteClusters.map { $0.mapToAbc() }.joined(separator: " ")
+        abc += " "
+        abc += trailingBarline.rawValue
+        return abc
+    }
+}
+
+extension Note: AbcSourceConverting {
+    fileprivate func abcSource() -> String {
+        var abc = ""
+        if let embellishment, let gracenotes = try? pitch.gracenotes(for: embellishment) {
+            abc += "{\(gracenotes.mapToAbc())}"
+        }
+        abc += pitch.abcSource()
+        return abc
+    }
+}
+
+extension Pitch: AbcSourceConverting {
+    fileprivate func abcSource() -> String {
+        switch self {
+        case .highA: "a"
+        case .highG: "g"
+        case .f: "f"
+        case .e: "e"
+        case .d: "d"
+        case .c: "c"
+        case .b: "B"
+        case .lowA: "A"
+        case .lowG: "G"
+        }
+    }
+}
+
+// extension Duration: AbcSourceConverting {
+//    fileprivate func abcSource() -> String {
+//
+//    }
+// }
+
+private protocol AbcSourceConverting {
+    func abcSource() -> String
+}
+
+private extension Array where Element: AbcSourceConverting {
+    func mapToAbc(joined: String = "") -> String {
+        map { $0.abcSource() }.joined(separator: joined)
     }
 }
