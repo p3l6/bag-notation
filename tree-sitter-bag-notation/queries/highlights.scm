34d09f3a20fba750d44c31e07ff7558679780f01
Paul Landers <paul@thelanders.family>
2024-11-25 20:46:13 -0500

    Add some highlights

---
+++ tree-sitter-bag-notation/queries/highlights.scm
@@ -0,0 +1,19 @@
+; highlights.scm
+
+(comment) @comment
+(tail_comment) @comment
+
+(field_label) @keyword
+(shorthand_label) @keyword
+(field_value) @string
+
+"---\n" @operator
+"(" @operator
+")" @operator
+(barline) @operator
+
+(embellishment) @variable
+(pitch) @constant
+(duration) @operator
+(connector) @operator
+
